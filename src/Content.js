855d056f8d4fc872ef1134d0279f649d7b2083b4
00yatejb <jrbyates@gmail.com>
2023-01-02 05:42:33 +0000

    Update Content.js

--- src/Content.js
+++ src/Content.js
@@ -6,21 +6,21 @@ const Content = [
 
 {
     id: 1,
-    title: 'Title 1', 
+    title: 'Coaching', 
     body: 'Ut mauris libero, faucibus ac dui in, molestie dictum urna. Maecenas in arcu ante. Phasellus eget urna nec purus mattis aliquet. Donec porta elementum sapien, eget mattis leo scelerisque ut. Fusce molestie quam quis turpis tempus, a consequat massa convallis. Pellentesque ipsum tellus, mollis ac eleifend id, efficitur nec tellus.', 
     image: coachingChart,
 },
 
 {
     id: 2,
-    title: 'Title 2', 
+    title: 'Mentoring', 
     body: 'Ut mauris libero, faucibus ac dui in, molestie dictum urna. Maecenas in arcu ante. Phasellus eget urna nec purus mattis aliquet. Donec porta elementum sapien, eget mattis leo scelerisque ut. Fusce molestie quam quis turpis tempus, a consequat massa convallis. Pellentesque ipsum tellus, mollis ac eleifend id, efficitur nec tellus.', 
     image: graduationPic,
 },
 
 {
     id: 3,
-    title: 'Title 3', 
+    title: 'Project Management', 
     body: 'Ut mauris libero, faucibus ac dui in, molestie dictum urna. Maecenas in arcu ante. Phasellus eget urna nec purus mattis aliquet. Donec porta elementum sapien, eget mattis leo scelerisque ut. Fusce molestie quam quis turpis tempus, a consequat massa convallis. Pellentesque ipsum tellus, mollis ac eleifend id, efficitur nec tellus.', 
     image: progressionPic,
 },
